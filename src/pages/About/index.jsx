b7aa68ae1f011d0686345ad54b3bf2ba94f5e56e
ISC-EIbarra <EdsonP4100@gmail.com>
2024-02-09 13:36:57 -0800

    Update about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -12,22 +12,25 @@ function About() {
         <div className="flex flex-1 items-center justify-center">
           <div className="container bg-white m-4 md:w-3/6 p-4 rounded-lg">
             <p className="text-base text-wrap font-normal">
-              Hola!, soy Edson Ibarra, desarrollador Frontend Jr, actualmente
-              poseo 4 meses de experiencia. Cuento con experiencia en algunas
-              areas de desarrollo, incluido la documentación del código del
-              proyecto. Desarrollo proyectos por mi cuenta, los cuales se
-              encuentran añadidos en este portafolio.
+              Hola, soy Edson Ibarra, un desarrollador Frontend Jr con 4 meses
+              de experiencia. Durante este tiempo, he adquirido habilidades en
+              diversas áreas del desarrollo, también he tenido participación en
+              la documentación de código de proyectos. Además de trabajar en
+              proyectos profesionales, también desarrollo proyectos por cuenta
+              propia, algunos de los cuales encontrarás en este portafolio.
             </p>
             <h1 className="text-lg font-medium mt-4 text-center ">
               Habilidades
             </h1>
-            <div className="flex flex-wrap gap-2 mt-4">
+            <div className="flex flex-wrap gap-2 py-4 justify-center  ">
               {data.map((info) => (
                 <Button
-                  className="flex gap-4 mt-2 items-center justify-center"
+                  color="default"
+                  variant="ghost"
+                  className="flex gap-4 items-center justify-center"
                   key={info.id}
                 >
-                  {info.name}
+                  <p className="font-medium">{info.name}</p>
                   <img className="h-8" src={info.icon} alt={info.id} />
                 </Button>
               ))}
